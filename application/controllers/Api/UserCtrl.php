2511aeaf903c697b877c634334fe6a69bb4fb6b2
Pakkapon Phongthawee <me@pureapp.in.th>
2017-05-29 23:42:41 +0700

    import: phpass for password hash

--- application/controllers/Api/UserCtrl.php
+++ application/controllers/Api/UserCtrl.php
@@ -11,8 +11,22 @@ class UserCtrl extends CI_Controller {
   * redirect traveller to promise land
   * @method index
   **/
-  public function index($hashcode)
+  public function index()
 	{
 
 	}
+  public function create()
+  {
+    $username = $this->input->post("username");
+    $email = $this->input->post("email");
+    $password = $this->input->post("password");
+    if(empty($user) || empty($email)){
+      //render error
+    }
+    if(empty($password)){
+      //create random string?
+      //$password =
+    }
+    //encrypted password
+  }
 }
