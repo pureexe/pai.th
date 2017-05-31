5f061e6c6c2397f0c6e3e5c1c677a9ff37440bf0
Pakkapon Phongthawee <me@pureapp.in.th>
2017-05-31 22:02:45 +0700

    draft css

---
+++ assets/js/user_manage.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '#user_manage',
+  data: {
+    page: "loading",
+    message: 'Hello Vue!',
+    email: "place holder",
+    fullURL: "",
+    shortening: false,
+  }
+});
