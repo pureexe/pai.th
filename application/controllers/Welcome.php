9242391db2c392c7c72c9d50dcb1f84efdccc301
Pakkapon Phongthawee <me@pureapp.in.th>
2017-06-02 14:48:23 +0700

    Fix: welcome controller

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,12 +18,8 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function __construct() {
-	 	parent::__construct();
- 	}
 	public function index()
 	{
-		$this->form_validation->set_rules('username','username','required');
 		$this->load->view('welcome_message');
 	}
 }
