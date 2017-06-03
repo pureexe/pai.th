32c0cde16a8f41fa73b03d291dff96ba512be247
Pakkapon Phongthawee <me@pureapp.in.th>
2017-06-03 17:23:02 +0700

    production configuration

--- application/config/subth.php
+++ application/config/subth.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $config["shorten_quota"] = 33;
-$config["http_redirect_code"] = 302 ; //Don't forgot change to 301 before prodcution
+$config["http_redirect_code"] = 301;
 $config['shorten_whitelist'] = array(
   'tafasu.com',
   'www.tafasu.com',
