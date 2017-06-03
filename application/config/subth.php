3a31499864b315858ae4aa6f0eb0b346649849f6
Pakkapon Phongthawee <me@pureapp.in.th>
2017-06-03 18:03:17 +0700

    update: Redirect Code

--- application/config/subth.php
+++ application/config/subth.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $config["shorten_quota"] = 33;
-$config["http_redirect_code"] = 301;
+$config["http_redirect_code"] = ENVIRONMENT !== 'production'?302:301;
 $config['shorten_whitelist'] = array(
   'tafasu.com',
   'www.tafasu.com',
