4300c0404ca549f80bb40b8c7e5a96e69a5a9b7b
Pakkapon Phongthawee <me@pureapp.in.th>
2017-06-04 22:35:14 +0700

    drafting controller

---
+++ application/controllers/Exporter.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* @class: RedirectorCtrl
+**/
+class Exporter extends CI_Controller {
+  public function __construct()
+  {
+    parent::__construct();
+  }
+  public function firebase()
+  {
+
+  }
+  public function jekyll()
+  {
+    
+  }
+}
